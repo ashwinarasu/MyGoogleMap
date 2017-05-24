f41ac938fb1e2b124afa67d7a693e8629916248b
ashwinarasu <arasuashwin@gmail.com>
2017-05-24 11:59:04 -0700

    birthplace marker and attempted current location marker

--- MyGoogleMap/app/src/main/java/com/example/arasua6707/mygooglemap/MapsActivity.java
+++ MyGoogleMap/app/src/main/java/com/example/arasua6707/mygooglemap/MapsActivity.java
@@ -39,8 +39,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng dearborn = new LatLng(42.3001807, -83.2437341);
+        mMap.addMarker(new MarkerOptions().position(dearborn).title("Birthplace"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dearborn));
+
+        //Add marker dot at current location and move the camera
+
+      //use getLastLocation() ??
+
+
+    }
     }
 }
